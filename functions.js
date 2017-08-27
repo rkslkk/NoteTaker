3504d64fa59e7d8c65b82cac31e08e315e25a671
Erik Slikkerveer <erikslikk41@gmail.com>
2017-08-27 10:22:21 -0700

    local storage

--- functions.js
+++ functions.js
@@ -8,16 +8,6 @@ function check_web_storage_support() {
     }
 }
 
-function display_saved_note() {
-    if(check_web_storage_support() == true) {
-        result = localStorage.getItem('note');
-    }
-    if(result === null) {
-        result = "No note saved";
-    }
-    document.getElementById('area').value = result;
-}
-
 function save() {
     if(check_web_storage_support() == true) {
         var area = document.getElementById("area");
